496ab80e90f9e8db433df968af46d7dc5a71b855
neverlosepain <neverlosepain@gmail.com>
2023-10-13 22:43:35 +0200

    fix some issue

--- _next/static/9-LrS3KTw4CwjTRDWMBTS/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/517-f1b7d3f9a815d543.js","static/chunks/pages/index-0cbcc6bd60f132d0.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
